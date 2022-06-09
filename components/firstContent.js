8d9a49446594e29e9d7d1de096491672dc5751a1
duyvo689 <vothanhduy689@gmail.com>
2022-06-10 00:34:21 +0700

    make base content Order and sales

--- components/firstContent.js
+++ components/firstContent.js
@@ -1,25 +1,57 @@
 import Business from "./firstContent/business"
 import Event from "./firstContent/event"
+import Congratulation from "./firstContent/congratulation"
+
 export default function FirstContent() {
     return (
         <div className="grid grid-cols-3 gap-6">
-            <div className="col-span-1 bg-green-200">
-                Giua 1
+            <div className="col-span-1">
                 <div className="grid grid-rows-3 gap-6">
                     <div className="grid grid-cols-2 gap-6">
-                        <div className="col-span-1 bg-yellow-200">
-                            giua -giua
+                        <div className="col-span-2">
+                            <Congratulation />
                         </div>
-                        <div className="col-span-1 bg-yellow-200">
-                            giua -giua
-                        </div>
+
                     </div>
                     <div className="grid grid-cols-2 gap-6">
-                        <div className="col-span-1 bg-yellow-200">
-                            giua -giua
+                        <div className="col-span-1 bg-white shadow rounded p-5 flex flex-col gap-3">
+                            <div className="flex justify-between">
+                                <img src="/img/icon-order.svg" alt="" />
+                                <svg width="5" height="14" viewBox="0 0 5 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M2.43787 5.25C1.47537 5.25 0.687866 6.0375 0.687866 7C0.687866 7.9625 1.47537 8.75 2.43787 8.75C3.40037 8.75 4.18787 7.9625 4.18787 7C4.18787 6.0375 3.40037 5.25 2.43787 5.25ZM2.43787 0C1.47537 0 0.687866 0.7875 0.687866 1.75C0.687866 2.7125 1.47537 3.5 2.43787 3.5C3.40037 3.5 4.18787 2.7125 4.18787 1.75C4.18787 0.7875 3.40037 0 2.43787 0ZM2.43787 10.5C1.47537 10.5 0.687866 11.2875 0.687866 12.25C0.687866 13.2125 1.47537 14 2.43787 14C3.40037 14 4.18787 13.2125 4.18787 12.25C4.18787 11.2875 3.40037 10.5 2.43787 10.5Z" fill="#435971" />
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M2.43787 5.25C1.47537 5.25 0.687866 6.0375 0.687866 7C0.687866 7.9625 1.47537 8.75 2.43787 8.75C3.40037 8.75 4.18787 7.9625 4.18787 7C4.18787 6.0375 3.40037 5.25 2.43787 5.25ZM2.43787 0C1.47537 0 0.687866 0.7875 0.687866 1.75C0.687866 2.7125 1.47537 3.5 2.43787 3.5C3.40037 3.5 4.18787 2.7125 4.18787 1.75C4.18787 0.7875 3.40037 0 2.43787 0ZM2.43787 10.5C1.47537 10.5 0.687866 11.2875 0.687866 12.25C0.687866 13.2125 1.47537 14 2.43787 14C3.40037 14 4.18787 13.2125 4.18787 12.25C4.18787 11.2875 3.40037 10.5 2.43787 10.5Z" fill="white" fillOpacity="0.3" />
+                                </svg>
+                            </div>
+                            <div className="">
+                                <h3 className="text-gray-500">Order</h3>
+                                <span className="text-gray-400 italic font-thin text-xl">$1,286</span>
+                            </div>
+
+                            <div className="flex items-center">
+                                <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M10 5L8.94588 3.94588L5.74549 7.14627V0H4.25451V7.14627L1.05412 3.94588L0 5L5 10L10 5Z" fill="#FF3E1D" />
+                                </svg>
+                                <span className="text-red-500 ml-2">$1,286</span>
+                            </div>
+
                         </div>
-                        <div className="col-span-1 bg-yellow-200">
-                            giua -giua
+                        <div className="col-span-1 bg-white shadow rounded p-5 flex flex-col gap-3">
+                            <p>Sales</p>
+                            <div className="">
+                                <h3>482k</h3>
+                                <span>+34%</span>
+                            </div>
+                            <div className="">
+                                <p>Sales Target</p>
+                                <svg width="90" height="8" viewBox="0 0 90 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <rect width="89.6857" height="8" rx="4" fill="#435971" />
+                                    <rect width="89.6857" height="8" rx="4" fill="white" fillOpacity="0.9" />
+                                </svg>
+                                <span>78%</span>
+
+                            </div>
+
+
                         </div>
                     </div>
 
